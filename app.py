a6a52d06fa4d2e4c99ee44f7a338f5d23a777158
Raghuram1234 <raghuram.kanungo@gmail.com>
2026-03-29 18:46:59 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+print("hello world")
+print("hello world" * 10)
+name = "Raghuram"
+age = 11
+print (name, age)
+print (f"my name is {name} and my age is {age}")
+favourite_food = "Shahi Paneer"
+print (f"my favourite food is {favourite_food}")
+# A simple greeting program
+username = input("Raghuram: ")
+birth_year = input("2014? ")
+
+# We convert the input to a number (int) to do math
+age = 2026 - int(2014)
+
+print("Hello " + Raghuram + "!")
+print("You are " + str(age) + " years old.")
+(input("Enter your name: "))
